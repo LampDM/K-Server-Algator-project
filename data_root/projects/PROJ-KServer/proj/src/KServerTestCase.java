e4dab663ff06c9d013f15d0063622b8cbdd93c3b
Lamp <dmvoicestudios@gmail.com>
2019-07-10 11:41:12 +0200

    added all tests from thesis - TestSet1

--- data_root/projects/PROJ-KServer/proj/src/KServerTestCase.java
+++ data_root/projects/PROJ-KServer/proj/src/KServerTestCase.java
@@ -1,4 +1,7 @@
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.util.HashSet;
 import java.util.Random;
 import java.util.Set;
@@ -116,7 +119,6 @@ public class KServerTestCase extends AbstractTestCase {
                     ks[i][1] = Integer.parseInt(cords[i].split(",")[1]);
                 }
                 break;
-
             default:
                 throw new IllegalArgumentException("Initial server position invalid argument! Try RANDOM!");
         }
@@ -138,16 +140,86 @@ public class KServerTestCase extends AbstractTestCase {
                     req[i][1] = Integer.parseInt(cords[i].split(",")[1]);
                 }
                 break;
+            //The margins are 20% and 80% for x and y
+            case "CORNER":
+                for (int i = 0; i < req.length; i++) {
+                    //70% of reqs appear in the corner
+                    if (i < 0.7 * req.length) {
+                        req[i][0] = rand.nextInt((int) Math.round(x * 0.2));
+                        req[i][1] = rand.nextInt((int) Math.round(y * 0.2));
+                        //30% of reqs appear elsewhere
+                    } else {
+                        int xh = (int) (x * 0.2);
+                        int yh = (int) (y * 0.2);
+                        //Random number from xh to x and yh to y
+                        req[i][0] = (int) (rand.nextInt(x - xh) + xh);
+                        req[i][1] = (int) (rand.nextInt(y - yh) + yh);
+                    }
+                }
+                break;
+            //The margins are 20% then 45% then 35% for x and y
+            case "WAVE":
+                for (int i = 0; i < req.length; i++) {
+
+                    //20% of reqs in the corner
+                    if (i < 0.2 * req.length) {
+                        req[i][0] = rand.nextInt((int) Math.round(x * 0.2));
+                        req[i][1] = rand.nextInt((int) Math.round(y * 0.2));
+                    } else //60% of reqs in the center wave
+                    if ((i > 0.2 * req.length) && (i < 0.8 * req.length)) {
+                        int xh = (int) (x * 0.2);
+                        int yh = (int) (y * 0.2);
+                        int xd = (int) (x * 0.7);
+                        int yd = (int) (y * 0.7);
+                        //Random number from xh to x and yh to y
+                        req[i][0] = (int) (rand.nextInt(xd - xh) + xh);
+                        req[i][1] = (int) (rand.nextInt(yd - yh) + yh);
+                        //20% of reqs in the last wave
+                    } else {
+                        int xh = (int) (x * 0.7);
+                        int yh = (int) (y * 0.7);
+                        //Random number from xh to x and yh to y
+                        req[i][0] = (int) (rand.nextInt(x - xh) + xh);
+                        req[i][1] = (int) (rand.nextInt(y - yh) + yh);
+                    }
+                }
+                break;
+            case "FILE":
+                String filename = reqstype.split(" ")[1];
+                String testfile = path + File.separator + filename;
+                        
+                File file = new File(testfile);
+                BufferedReader br = null;
+                try {
+                    br = new BufferedReader(new FileReader(file));
+                    String st = br.readLine();
+                    String[] rqs = st.split(" ");
+                    for(int i = 0;i<req.length;i++){
+                        String[] duo = rqs[i].split(",");
+                        req[i][0] = Integer.parseInt(duo[0]);
+                        req[i][1] = Integer.parseInt(duo[1]);
+                    }
+                    
+                } catch (Exception ex) {
+                    System.out.println(ex);
+                    //TODO what to do with exception?
+                }
+
+                break;
+
             default:
                 throw new IllegalArgumentException("Request appearance method invalid argument! Try RANDOM!");
         }
         //For debugging purposes
         //KServerTools.printSomething(ks);
         //KServerTools.printSomething(req);
-        
+
         KServerTestCase kServerTestCase = new KServerTestCase();
-        kServerTestCase.setInput(new KServerInput(ks, req, x, y));
-        kServerTestCase.getInput().setParameters(inputParameters);
+
+        kServerTestCase.setInput(
+                new KServerInput(ks, req, x, y));
+        kServerTestCase.getInput()
+                .setParameters(inputParameters);
 
         //Use indexes for solution format, so for example 0 means first servers, 1 means second etc.
         String optimal_solution = "";
@@ -160,7 +232,8 @@ public class KServerTestCase extends AbstractTestCase {
             //TODO what happens if error?
         }
 
-        kServerTestCase.setExpectedOutput(new KServerOutput(optimal_solution, ks, req));
+        kServerTestCase.setExpectedOutput(
+                new KServerOutput(optimal_solution, ks, req));
 
         return kServerTestCase;
 
@@ -173,6 +246,9 @@ public class KServerTestCase extends AbstractTestCase {
         if (spostype.startsWith("INLINE")) {
             return "INLINE";
         }
+        if (spostype.startsWith("FILE")) {
+            return "FILE";
+        }
         return null;
     }
 
@@ -182,6 +258,15 @@ public class KServerTestCase extends AbstractTestCase {
         }
         if (reqstype.startsWith("INLINE")) {
             return "INLINE";
+        }
+        if (reqstype.startsWith("CORNER")) {
+            return "CORNER";
+        }
+        if (reqstype.startsWith("WAVE")) {
+            return "WAVE";
+        }
+        if (reqstype.startsWith("FILE")) {
+            return "FILE";
         }
         return null;
     }
--- data_root/projects/PROJ-KServer/proj/src/KServerTools.java
+++ data_root/projects/PROJ-KServer/proj/src/KServerTools.java
@@ -1,9 +1,11 @@
+
+import java.util.Random;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author dani
@@ -14,14 +16,14 @@ public class KServerTools {
 
     }
 
-    public static void printSomething(int[][] something){
+    public static void printSomething(int[][] something) {
         System.out.println("");
-        for (int[] el : something){
-            System.out.print("("+el[0]+","+el[1]+") ");
+        for (int[] el : something) {
+            System.out.print("(" + el[0] + "," + el[1] + ") ");
         }
         System.out.println("");
     }
-    
+
     public static double calcDist(double x1, double y1, double x2, double y2) {
         return Math.sqrt(Math.pow((x1 - x2), 2) + Math.pow((y2 - y1), 2));
     }
@@ -29,28 +31,28 @@ public class KServerTools {
     public static double distTravelled(String movements, int[][] servers, int[][] requests) {
         double dist = 0.0;
         //TODO turn this into some function or find more elegant solution
-        int [][] ks = new int[servers.length][servers[0].length];
-        int [][] req = new int[requests.length][requests[0].length];
-        
+        int[][] ks = new int[servers.length][servers[0].length];
+        int[][] req = new int[requests.length][requests[0].length];
+
         //Copying arrays
-        for(int i =0;i<servers.length;i++){
+        for (int i = 0; i < servers.length; i++) {
             ks[i][0] = servers[i][0];
             ks[i][1] = servers[i][1];
         }
-        
-        for(int i =0;i<requests.length;i++){
+
+        for (int i = 0; i < requests.length; i++) {
             req[i][0] = requests[i][0];
             req[i][1] = requests[i][1];
         }
 
         String[] movs = movements.split(",");
-        
+
         for (int i = 0; i < req.length; i++) {
             //server servers[movs[i]] to requests[i], change position of servers[movs[i]]
             int mov = Integer.parseInt(movs[i]);
-            dist += (calcDist(ks[mov][0],ks[mov][1],req[i][0],req[i][1]));
-            ks[mov][0]=req[i][0];
-            ks[mov][1]=req[i][1];
+            dist += (calcDist(ks[mov][0], ks[mov][1], req[i][0], req[i][1]));
+            ks[mov][0] = req[i][0];
+            ks[mov][1] = req[i][1];
         }
         return dist;
     }
